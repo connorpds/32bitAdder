1dd7108a5c8430708d5b2ae1105fd06a625a5d69
Jaredh01 <jaredh01@comcast.net>
2020-01-22 23:03:41 -0600

    working test bench

--- CLA_4_tb.v
+++ CLA_4_tb.v
@@ -4,25 +4,25 @@ module CLA_4_tb;
 	reg Cin;
 	wire [3:0] Sum;
 	wire Cout;
-	reg i;
-	reg j;
+	reg [3:0] i;
+	reg [3:0] j;
 	
-	CLA_4 cla (.a(A), .b(B), .c_in(Cin), .s(Sum), .c_out(cout));
+	CLA_4 cla (.a(A), .b(B), .c_in(Cin), .s(Sum), .c_out(Cout));
 	
 	initial begin
-		//$monitor("A=%b B=%b, Cin=%b -> Sum=%b, Cout=%b")
+		$monitor("A=%b B=%b, Cin=%b -> Sum=%b, Cout=%b", A, B, Cin, Sum, Cout);
 		
 		for(i = 4'b0000; i < 4'b1111; i = i + 4'b0001) begin
 			for (j = 4'b0000; j < 4'b1111; j = j + 4'b0001) begin
-				#1 
+				#100
 				A = i; B=j; Cin=1'b0;
-				#1
+				#100
 				if (Sum != A+B) begin
 				   $finish(1);
 				end
-				#1 
+				#100 
 				Cin = 1'b1;
-				#1
+				#100
 				if (Sum != A+B+Cin) begin
 				   $finish(1);
 				end
