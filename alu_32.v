afab37be70b38028919f8638df1c4a672d5d7d38
Jaredh01 <jaredh01@comcast.net>
2020-01-26 20:49:25 -0600

    Addition of logic ops and shift ops

--- alu_32.v
+++ alu_32.v
@@ -11,22 +11,28 @@ wire[31:0] B_adder;
 wire[31:0] and_out;
 wire[31:0] or_out;
 wire[31:0] xor_out;
+wire[31:0] sll_out;
+wire[31:0] srl_out;
+wire[31:0] sra_out;
 
 wire c_out;
 wire c_in;
-wire c_in_sel;
+wire add_opcode;
 wire overflow;
 
 //Determine inputs to the adder
+xnor_gate_6to1 determine_add(.x(opcode), .y(6b'100000), .z(add_opcode));
 not_gate_32 b_inverter(.x(B), .z(B_inv));
-mux_32 b_sel(.sel(B_sel), .src0(B), .src1(B_inv), .z(B_adder));
-mux (.sel(c_in_sel), .src0(1'b0), .src1(1'b1), .z(c_in));
+mux_32 b_mux(.sel(add_opcode), .src0(B_inv), .src1(B), .z(B_adder));
+mux c_in_mux(.sel(add_opcode), .src0(1'b1), .src1(1'b0), .z(c_in));
 
 //Perform add/subtract
 CLA_32 adder(.a(A), .b(B_adder), .c_in(c_in), .s(adder_out), .c_out(c_out), .overflow(overflow));
 
 //Shifters
-
+sll_32 sll(.A(A), .B(B), .out(sll_out));
+srl_32 srl(.A(A), .B(B), .out(srl_out));
+sra_32 sra(.A(A), .B(B), .out(sra_out));
 
 //Logical ops
 and_gate_32 andfunc(.x(A), .y(B), .z(and_out));
@@ -35,12 +41,14 @@ xor_gate_32 xorfunc(.x(A), .y(B), .z(xor_out));
 
 //Set-ifs
 
+
+
 //MUX at the end for op selection, based off of DLX ALU func codes
 always @ ( A OR B OR opcode )
 	case (opcode)
-		6'b100 : out = //SLL
-		6'b110 : out = //SRL
-		6'b111 : out = //SRA
+		6'b100 : out = sll_out //SLL
+		6'b110 : out = srl_out //SRL
+		6'b111 : out = sra_out //SRA
 		6'b100000 : out = adder_out; //ADD
 		6'b100010 : out = adder_out; //SUB
 		6'b100100 : out = and_out; //AND
