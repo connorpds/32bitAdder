5aa11dabdb50258bf3450b26b15a44a49f294444
connorpds <connorselna2021@u.northwestern.edu>
2020-01-29 19:19:33 -0600

    setif testbench working

--- sets.v
+++ sets.v
@@ -54,13 +54,14 @@ endmodule
 
 //set less-than or equal
 module sle(
-  input wire [31:0] a,
+
   input wire zf,
+  input wire [31:0] a,
   output wire [31:0] sle
   );
 wire [31:0] slt_out;
 slt slt0(a,slt_out);
-or_gate(zf,slt_out[0],sle[0]);
+or_gate or0(zf,slt_out[0],sle[0]);
 
 
   assign sle[31:1] = {31{1'b0}};
