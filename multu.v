6a7461c7d69514a62c469facd09e67bb8cf489bb
connorpds <connorselna2021@u.northwestern.edu>
2020-02-05 17:22:42 -0600

    control fixed and compiles

--- multu.v
+++ multu.v
@@ -2,13 +2,13 @@
 `include "/shift_ops.v"
 
 module multu(
-	input reg [31:0] A;
-	input reg [31:0] B;
-	input reg clk;
+	input reg [31:0] A,
+	input reg [31:0] B,
+	input reg clk,
 	//Control inputs
-	input reg doMult;
-	input reg reset;
-	output wire [31:0] Out;
+	input reg doMult,
+	input reg reset,
+	output wire [31:0] Out
 );
 
 wire [31:0] mp_out; // Multiplicand out
@@ -23,16 +23,16 @@ wire a_s; // a_s = 0 -> perform add, otherwise shift
 wire prod_en; // enable product register loading from outside world
 
 // Map control
-multu_cont control(.doMult(doMult), .sysClk(clk), .mClk(clk), .reset(reset), .add0(add0), .a_s(a_s), .combined_reset(prod_en))
+multu_cont control(.doMult(doMult), .sysClk(clk), .mClk(clk), .reset(reset), .add0(add0), .a_s(a_s), .combined_reset(prod_en));
 
 // Perform add
 mux_32 det_adder_in(.sel(add0), .src0(mp_out), .src1(32'b0), .z(adder_in));
 CLA_32 adder(.A(adder_in), .B(prod_out[63:32]), .c_in(1'b0), .s(adder_out), .c_out(1'b0), .overflow(1'b0));
 
 // Determine input to product reg
-srl_64 shifted_prod(.A(prod_out), .B(64'b1), .out(shifted_prod));
-mux_n #(64) (.sel(a_s), .src0( {adder_out, prod_out[31:0]} ), src1( shifted_prod ), .z( add_shift_out ) );
-mux_n #(64) (.sel(prod_en), .src0(add_shift_out), .src1({32'b0, B}), .z(prod_in));
+srl_64 shifted_prod0(.A(prod_out), .B(64'b1), .out(shifted_prod));
+mux_n #(64) shift_mux(.sel(a_s), .src0( {adder_out, prod_out[31:0]} ), .src1( shifted_prod ), .z( add_shift_out ));
+mux_n #(64) load_mux(.sel(prod_en), .src0(add_shift_out), .src1({32'b0, B}), .z(prod_in));
 
 //Registers
 register_n #(64) product_reg( .clk(clk), .reset(1'b0), .wr_en(1'b1), .d(prod_out), .q(prod_in) );
--- multu_control.v
+++ multu_control.v
@@ -11,30 +11,35 @@ module multu_control
   input wire sysClk,
   input wire mClk,
   input wire reset, //high, reset all the things
+  input wire prod_LSB, //product LSB
   output wire add0, //0 is adding multiplicand, 1 is adding 0
   output wire a_s, //add or shift. 0 is add, 1 is shift
-  output wire combined_reset //0 is product[31:0] not updated externally, 1: product[31:0] is B
-  //NOT SURE EXACTLY WHAT TO DO HERE. THIS MAY BE BEST DONE OUTSIDE THE CONTROL.
+  output wire combined_reset, //0 is product[31:0] not updated externally, 1: product[31:0] is B
+  output wire mult_done
   );
 
   //-need memory to remember if it's been intialized. starts at 0. DONEZO
   //-need memory to count how many shifts have occurred ---basically done
   //-need memory to remember what last thing was
-reg doing_mult;
- //just for now
+wire doing_mult;
+reg ctrl_reset;
+
+//add0 is just !LSB
+not_gate notLSB(prod_LSB,add0);
+
+and_gate comb_rst(ctrl_reset,reset,combined_reset);
+
 
 //initialization: when doMult goes high, we decide to initialize. We will
 //probably do some resetting here, too.
-  reg init;
   always@ (posedge doMult)
     begin
-      init = 1;
-      doing_mult = 1;
+      ctrl_reset = 1;
     end
 
   always@ (negedge doMult)
     begin
-      init = 0;
+      ctrl_reset = 0;
     end
 
 //5 bit register and counter, adds 0 when add_count is 0, adds 1 when add_count is 1
@@ -42,16 +47,15 @@ reg doing_mult;
   wire [4:0] out5;
   wire next_op;
   wire a_s0; //a_s for internal use
-  wire mult_done;
 
   //memory to keep track of which op to perform
   not_gate not_a_s(a_s0,next_op); //e
-  register_n #(1) reg1(.clk(clk), .reset(combined_reset),.wr_en(doing_mult),.d(next_op),.q(a_s0));
+  register_n #(1) op_tracker(.clk(clk), .reset(combined_reset),.wr_en(doing_mult),.d(next_op),.q(a_s0));
 
 
 //ALTERNATE SOLN. 32 BIT ALL HIGH START REGISTER, KEEP RIGHT shifting
 //UNTIL LSB IS 0, THEN WE'VE SHIFTED A FULL 32 TIMES
-  register_n #(5) reg5(.clk(clk), .reset(combined_reset),.wr_en(a_s),.d(out5),.q(reg5out));
+  register_n #(5) counter_reg(.clk(clk), .reset(combined_reset),.wr_en(1'b1),.d(out5),.q(reg5out));
   add_5 incr(.a(reg5out),.b({4'b0000,a_s}),.s(out5));
   //checking if the register is all 1s, implying 32 shifts have occurred
   and_5 add5maxed(reg5out[0],reg5out[1],reg5out[2],reg5out[3],reg5out[4],mult_done);
@@ -62,4 +66,4 @@ reg doing_mult;
   not_gate stop_doing(mult_done, doing_mult); //turns doing_mult off
   mux mltdone_add_only(mult_done,a_s0,zero,a_s);
 
-  endmodule
+endmodule
