b503f7adbac79cd6e817c4d7dd0bae0e8f728e71
Jaredh01 <jaredh01@comcast.net>
2020-02-09 18:24:14 -0600

    Implenting signed multiply in datapath

--- mult.v
+++ mult.v
@@ -14,22 +14,30 @@ module mult(
 );
 
 wire [31:0] mp_out; // Multiplicand out
+wire [31:0] mp_out_not;
 wire [63:0] prod_out; // Product register out
 wire [31:0] adder_out;
 wire [31:0] adder_in;
 wire [63:0] shifted_prod;
 wire [63:0] add_shift_out;
 wire [63:0] prod_in;
+wire cin;
 wire add0; // add0=1 -> add 0 to product reg
 wire a_s; // a_s = 0 -> perform add, otherwise shift
 wire prod_en; // enable product register loading from outside world
+wire dosub; // dosub == 0 -> perform add, otherwise subtract
 
 // Map control
 multu_control control(.doMult(doMult), .sysClk(clk), .mClk(clk), .reset(reset), .add0(add0), .a_s(a_s), .combined_reset(prod_en), .mult_done(mult_done), .prod_LSB(prod_out[0]));
 
-// Perform add
-mux_32 det_adder_in(.sel(add0), .src0(mp_out), .src1(32'b0), .z(adder_in));
-CLA_32 adder(.a(adder_in), .b(prod_out[63:32]), .c_in(1'b0), .s(adder_out), .c_out(), .overflow());
+// setup for add
+not_gate_32 mp_not(.x(mp_out), .z(mp_out_not));
+mux_32 det_addorsub(.sel(dosub), .src0(mp_out), .src1(mp_out_not), .z(potential_adder_in));
+mux_32 det_adder_in(.sel(add0), .src0(potential_adder_in), .src1(32'b0), .z(adder_in));
+mux_32 det_cing(.sel(dosub), .src0(1'b0), .src1(.1'b1), .z(cin))
+
+//Do add
+CLA_32 adder(.a(adder_in), .b(prod_out[63:32]), .c_in(cin), .s(adder_out), .c_out(), .overflow());
 
 // Determine input to product reg
 srl_64 shifted_prod0(.A(prod_out), .B(64'b1), .out(shifted_prod));
