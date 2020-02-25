80ee97dac73eff817c057a3a055ce925f4ce5152
Jaredh01 <jaredh01@comcast.net>
2020-02-25 16:58:49 -0600

    Addition of jump_r

--- inst_fetch.v
+++ inst_fetch.v
@@ -7,10 +7,12 @@ module inst_fetch(
 	//input reg [31:0] a,
 	input wire [15:0] imm16,
 	input wire [25:0] jmp_imm26,
+	input wire [32:0] reg_imm32,
 	input wire clk,
 	//Control inputs
 	input wire branch,
 	input wire jmp,
+	input wire jmp_r,
 	input wire reset,
 	output reg [31:0] pc
 );
@@ -23,6 +25,7 @@ wire [31:0] branch_addr;
 wire [5:0] jmp_sign_extend;
 wire [15:0] branch_sign_extend;
 wire [31:0] temp_w_jump;
+wire [31:0] temp_w_branch;
 
 
 register_n PC_reg(.clk(clk), .reset(reset), .wr_en(1'b1), .d(pc_in), .q(pc_out) );
@@ -38,7 +41,8 @@ mux_n #(16) branch_extend_mux(.sel(imm16[15]), .src0(16'b0), .src1(16'b111111111
 
 //Combinational logic to determine pc_in
 mux_32 det_jmp (.sel(jmp), .src0(incr4_addr), .src1(jmp_addr), .z(temp_w_jump));
-mux_32 det_branch (.sel(branch), .src0(temp_w_jump), .src1(branch_addr), .z(pc_in));
+mux_32 det_branch (.sel(branch), .src0(temp_w_jump), .src1(branch_addr), .z(temp_w_branch));
+mux_32 det_jmp_r (.sel(jmp_r), .src0(temp_w_branch), .src1(reg_imm32), .z(pc_in));
 
 always @*
 	pc = pc_out; // continuous assign so we can have internal wire and output
