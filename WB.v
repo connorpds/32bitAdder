586d55189702d81bb781b7a110a44b114b3e31b4
Jaredh01 <jaredh01@comcast.net>
2020-02-25 18:24:31 -0600

    Mostly compiling WB

--- WB.v
+++ WB.v
@@ -1,53 +1,44 @@
+`include "extend.v"
+`include "lib/mux_32.v"
+`include "lib/and_gate.v"
 
 module WB(
-	input reg [31:0] mem_out,
-	input reg [31:0] ALU_out,
-	input reg [31:0] PC,
-	input reg [16:0] imm16,
-	input reg mem_to_reg,
-	input reg lhi,
-	input reg link,
-	input reg lb,
-	input reg lh,
-	input reg load_extend
+	input wire [31:0] mem_out,
+	input wire [31:0] ALU_out,
+	input wire [31:0] PC,
+	input wire [15:0] imm16,
+	input wire mem_to_reg,
+	input wire lhi,
+	input wire link,
+	input wire lb,
+	input wire lh,
+	input wire load_extend,
+	output reg WB_out
 );
 
-wire [31:0] mp_out; // Multiplicand out
-wire [31:0] mp_out_not;
-wire [63:0] prod_out; // Product register out
-wire [31:0] adder_out;
-wire [31:0] potential_adder_in;
-wire [31:0] adder_in;
-wire [63:0] shifted_prod;
-wire [63:0] add_shift_out;
-wire [63:0] prod_in;
-wire cin;
-wire add0; // add0=1 -> add 0 to product reg
-wire a_s; // a_s = 0 -> perform add, otherwise shift
-wire prod_en; // enable product register loading from outside world
-wire doSub; // dosub == 0 -> perform add, otherwise subtract
+wire [31:0] mem_out_lb;
+wire [31:0] mem_out_lh;
+wire [31:0] det_lb_out;
+wire [31:0] det_lh_out;
+wire [31:0] det_ALU_out;
+wire [31:0] det_PC_out;
+wire [31:0] WB_out_temp;
 
-// Map control
-multu_control control(.doMult(doMult), .sysClk(clk), .mClk(clk), .reset(reset), .add0(add0), .a_s(a_s), .doSub(doSub), .combined_reset(prod_en), .mult_done(mult_done), .prod_LSB(prod_out[0]));
 
-// setup for add
-not_gate_32 mp_not(.x(mp_out), .z(mp_out_not));
-mux_32 det_addorsub(.sel(doSub), .src0(mp_out), .src1(mp_out_not), .z(potential_adder_in));
-mux_32 det_adder_in(.sel(add0), .src0(potential_adder_in), .src1(32'b0), .z(adder_in));
-mux_32 det_cin(.sel(doSub), .src0(1'b0), .src1(1'b1), .z(cin));
+//Prepare the lb and lh versions of mem_out
+extend #(8) lb_extend(.ext_op(load_extend), .in(mem_out[7:0]), .out(mem_out_lb));
+extend #(16) lh_extend(.ext_op(load_extend), .in(mem_out[15:0]), .out(mem_out_lh));
 
-//Do add
-CLA_32 adder(.a(adder_in), .b(prod_out[63:32]), .c_in(cin), .s(adder_out), .c_out(), .overflow());
+//Decide on version of mem_out
+mux_32 det_lb(.sel(lb), .src0(mem_out), .src1(mem_out_lb), .z(det_lb_out));
+mux_32 det_lh(.sel(lh), .src0(det_lb_out), .src1(mem_out_lh), .z(det_lh_out));
 
-// Determine input to product reg
-srl_64 shifted_prod0(.A(prod_out), .B(64'b1), .out(shifted_prod));
-mux_n #(64) shift_mux(.sel(a_s), .src0( {adder_out, prod_out[31:0]} ), .src1( shifted_prod ), .z( add_shift_out ));
-mux_n #(64) load_mux(.sel(prod_en), .src0(add_shift_out), .src1({32'b0, b}), .z(prod_in));
+//Decide if using mem_out, ALU_out, PC, or extended imm16
+mux_32 det_ALU(.sel(mem_to_reg), .src0(ALU_out), .src1(det_lh_out), .z(det_ALU_out));
+mux_32 det_PC(.sel(link), .src0(det_ALU_out), .src1(PC), .z(det_PC_out));
+mux_32 det_imm16(.sel(lhi), .src0(det_PC_out), .src1( { imm16, 16'b0} ), .z(WB_out_temp));
 
-//Registers
-register_n #(64) product_reg( .clk(clk), .reset(1'b0), .wr_en(1'b1), .d(prod_in), .q(prod_out) );
-register_n #(32) mp_reg( .clk(clk), .reset(1'b0), .wr_en(prod_en), .d(a), .q(mp_out));
-
-assign out = prod_out;
-
+always @ *
+	WB_out = WB_out_temp;
+	
 endmodule
