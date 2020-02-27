f476fbe3e0e2b97339ef0e81914739ac5d1378d0
Jaredh01 <jaredh01@comcast.net>
2020-02-26 21:24:20 -0600

    Amending to suppport JR

--- inst_fetch_tb.v
+++ inst_fetch_tb.v
@@ -2,14 +2,16 @@ module inst_fetch_tb;
 	//reg [31:0] a,
 	reg [15:0] imm16;
 	reg [25:0] jmp_imm26;
+	reg [31:0] reg_imm32;
 	reg clk;
 	//Control inputs
 	reg branch;
 	reg jmp;
+	reg jmp_r;
 	reg reset;
 	wire [31:0] pc;
 
-	inst_fetch fetch_undertest (.imm16(imm16), .jmp_imm26(jmp_imm26), .clk(clk), .branch(branch), .jmp(jmp), .reset(reset), .pc(pc));
+	inst_fetch fetch_undertest (.imm16(imm16), .jmp_imm26(jmp_imm26), .reg_imm32(reg_imm32), .jmp_r(jmp_r), .clk(clk), .branch(branch), .jmp(jmp), .reset(reset), .pc(pc));
 
 	initial begin
 		$monitor("imm16=%h, jmp_imm26=%h, clk=%b, branch=%b, jmp=%b, reset=%b -> pc=%h", imm16, jmp_imm26, clk, branch, jmp, reset, pc);
@@ -18,6 +20,7 @@ module inst_fetch_tb;
 		reset=1'b1;
 		clk=1'b1;
 		jmp=1'b0;
+		jmp_r=1'b0;
 		branch=1'b0;
 		#4
 		reset=1'b0;
