307744a44873c965f9231064666688fe580f606d
Jaredh01 <jaredh01@comcast.net>
2020-02-26 21:24:32 -0600

    Fixing linking address

--- WB.v
+++ WB.v
@@ -23,7 +23,10 @@ wire [31:0] det_lh_out;
 wire [31:0] det_ALU_out;
 wire [31:0] det_PC_out;
 wire [31:0] WB_out_temp;
+wire [31:0] PC_plus_8;
 
+//Calculate PC+8 for jumping and linking to store in R31
+CLA_32 incr4_calc(.a(PC), .b(32'b1000), .c_in(1'b0), .s(PC_plus_8), .c_out(),.overflow());
 
 //Prepare the lb and lh versions of mem_out
 extend #(8) lb_extend(.ext_op(load_extend), .in(mem_out[7:0]), .out(mem_out_lb));
@@ -35,7 +38,7 @@ mux_32 det_lh(.sel(lh), .src0(det_lb_out), .src1(mem_out_lh), .z(det_lh_out));
 
 //Decide if using mem_out, ALU_out, PC, or extended imm16
 mux_32 det_ALU(.sel(mem_to_reg), .src0(ALU_out), .src1(det_lh_out), .z(det_ALU_out));
-mux_32 det_PC(.sel(link), .src0(det_ALU_out), .src1(PC), .z(det_PC_out));
+mux_32 det_PC(.sel(link), .src0(det_ALU_out), .src1(PC_plus_8), .z(det_PC_out));
 mux_32 det_imm16(.sel(lhi), .src0(det_PC_out), .src1( { imm16, 16'b0} ), .z(WB_out_temp));
 
 always @ *
