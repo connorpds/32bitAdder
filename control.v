f1561834d0f6abd1ce5eccec05f0962db09b00f5
Jaredh01 <jaredh01@comcast.net>
2020-03-10 22:10:39 -0500

    Fixing immediate extending on unsigned

--- control.v
+++ control.v
@@ -98,9 +98,13 @@ always @ *
 //Setting imm_extend
 always @ *
 	case(inst[31:26])
+		6'h9: func_code <= 6'h21; //ADDUI
+		6'hb: func_code <= 6'h23; //SUBUI
 		6'hc: imm_extend<=1'b0; //ANDI
 		6'hd: imm_extend<=1'b0; //ORI
 		6'he: imm_extend<=1'b0; //XORI
+		6'h21: imm_extend<=1'b0; //ADDU
+		6'h23: imm_excend<=1'b0; //SUBU
 		default: imm_extend<=1'b1;
 	endcase
 
